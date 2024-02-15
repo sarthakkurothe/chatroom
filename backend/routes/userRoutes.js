610a14a53088f68ebd5c037a0bf88683e1fb1b31
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-02-15 16:19:38 +0530

    search user and create chat apis

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.route("/").get(protect, allUsers);
 router.route("/").post(registerUser);
-router.post("/login");
+router.post("/login", authUser);
 
 module.exports = router;
